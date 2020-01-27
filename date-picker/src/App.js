081ee4ace54459e3af246a265ad9ac970fd852e0
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-01-27 17:23:44 +0100

    first commit

---
+++ date-picker/src/App.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import DatePicker from './components/DatePicker';
+
+const Container = styled.div`
+  height: 100vh;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: 'Rubik', sans-serif;
+
+  h1 {
+    margin-bottom: 100px;
+    color: #212529;
+  }
+
+  .form-container {
+    padding: 40px;
+    width: 400px;
+  }
+
+  .input-container {
+    width: 350px;
+    padding: 25px;
+  }
+
+  .input-container::after {
+    content: "";
+    display: block;
+    border: 1px solid black;
+    width: 100%;
+    border-color: #555555;
+    opacity: 0.6;
+    transition: all 0.5s;
+  }
+
+  .input-birth:hover::after,
+  .input-container:hover::after {
+    border-color: #57b846;
+  }
+
+  .submit-container {
+    width: 350px;
+    padding: 25px;
+  }
+
+  .input {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 9px;
+    font-weight: 600;
+    outline: none;
+    border: none;
+    font-size: 16px;
+  }
+
+  input::placeholder {
+    color: #555555;
+    opacity: 0.6;
+  }
+
+  .footer {
+    margin-top: 200px;
+    font-size: 14px;
+  }
+
+  .submit {
+    border-radius: 24px;
+    padding: 14px;
+    background-color: #57b846;
+    color: #fff;
+    text-transform: uppercase;
+    transition: all 0.4s;
+  }
+
+  .submit:hover {
+    background-color: #212529;
+    opacity: 0.9;
+  }
+
+  .birthdate {
+    width: 100%;
+  }
+
+  .input-birth {
+    position: relative;
+    width: 350px;
+    padding: 25px;
+  }
+
+  .input-birth::after {
+    content: "";
+    display: block;
+    border: 1px solid black;
+    width: 100%;
+    border-color: #555555;
+    opacity: 0.6;
+    transition: all 0.5s;
+  }
+
+  .datepicker-dropdown {
+    position: absolute;
+    right: 25px;
+    cursor: pointer;
+  }
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showDatePicker: false,
+    };
+  }
+
+  onClick = () => {
+    const datePicker = this.state.showDatePicker;
+
+    if (datePicker === false) {
+      this.setState({ showDatePicker: true })
+    } else {
+      this.setState({ showDatePicker: false })
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.showDatePicker ? <DatePicker/> : null}
+        <div className="form-container">
+          <h1>Welcome</h1>
+          <form>
+            <div className="input-container">
+              <input
+                className="input name"
+                type="text"
+                placeholder="Name"
+                />
+            </div>
+            <div className="input-birth">
+              <input 
+                className="input birthdate"
+                type="text"
+                placeholder="Birthdate"
+              />
+              <i className="material-icons datepicker-dropdown" onClick={this.onClick}>date_range</i>
+            </div>
+            <div className="input-container">
+              <input 
+                className="input password"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="submit-container">
+            <input 
+              className="input submit"
+              type="submit"
+            />
+            </div>
+          </form>
+          <div className="footer">
+            <p>This is a fake page to illustrate the datepicker</p>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ date-picker/src/components/DatePicker.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-family: 'Heebo', sans-serif;
+  font-weight: 500;
+  background-color: #fff;
+  position: absolute;
+  width: 352px;
+  left: 50%;
+  margin-left: -175px;
+  top: 50%;
+  margin-top: -67px;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+
+  /* ----------------------- HEADER ----------------------- */
+
+  header {
+    height: 110px;
+    background-color: #6200EE;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+  }
+  
+  .header-top {
+    height: 45px;
+  }
+
+  .header-bottom {
+    height: 55px;
+  }
+  
+  .header-top p {
+    padding: 18px;
+    text-transform: uppercase;
+    font-size: 10px;
+    letter-spacing: 2px;
+    color: #fff;
+    margin-block-start: 0px;
+    margin-block-end: 0px;
+  }
+
+  .header-bottom h1 {
+    font-weight: 500;
+    color: #fff;
+    padding: 18px;
+    margin-block-start: 0px;
+    margin-block-end: 0px;
+  }
+
+  /* ----------------------- MAIN ----------------------- */
+  main {
+    height: 320px;
+    background-color: #fff;
+  }
+
+  main .year {
+    display: flex;
+    color: #666666;
+  }
+
+  main .year p {
+    font-size: 14px;
+    margin-block-start: 0px;
+    margin-block-end: 0px;
+    padding: 18px 4px 18px 18px;
+  }
+
+  main .year .down-arrow,
+  main .year .left-arrow,
+  main .year .right-arrow {
+    line-height: 54px;
+    cursor: pointer;
+  }
+
+  main .year .down-arrow {
+    margin-right: 125px;
+  }
+
+  main .year .left-arrow {
+    margin-right: 24px;
+  }
+
+  main .days-of-the-week {
+    display: flex;
+    justify-content: space-around;
+    font-size: 14px;
+    color: #666666;
+    margin-bottom: 12px;
+  }
+
+  main .days {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  main .days .day {
+    margin: 12px 0px;
+    cursor: pointer;
+  }
+
+  /* ----------------------- FOOTER ----------------------- */
+
+  footer {
+    height: 70px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+
+  footer .buttons {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  footer .ok,
+  footer .cancel {
+    line-height: 70px;
+    font-weight: 600;
+    color: #6200EE;
+    cursor: pointer;
+    text-transform: uppercase;
+  }
+
+  footer .ok {
+    margin-right: 24px;
+  }
+
+  footer .cancel {
+    margin-right: 40px;
+  }
+`;
+
+class DatePicker extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      daysOfTheWeek: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+      daysInMonth: Array.from(Array(7).keys()),
+    };
+  }
+
+  render() {
+    let d = new Date().getUTCDate();
+    console.log(d)
+
+    return ReactDOM.createPortal (
+      <Container>
+        <header>
+          <div className="header-top">
+            <p>select date</p>
+          </div>
+          <div className="header-bottom">
+            <h1>Mon, Nov 17</h1>
+          </div>
+        </header>
+        <main>
+          <div className="year">
+            <p>November 2018</p>
+            <i class="material-icons down-arrow">arrow_drop_down</i>
+            <i class="material-icons left-arrow">keyboard_arrow_left</i>
+            <i class="material-icons right-arrow">keyboard_arrow_right</i>
+          </div>
+          <div className="days-of-the-week">
+            {this.state.daysOfTheWeek.map(weekday => <div>{weekday}</div>)}
+          </div>
+          <div className="days">{this.state.daysInMonth.map(day => <div className="day">{day}</div>)}</div>
+          <div className="days">{this.state.daysInMonth.map(day => <div className="day">{day}</div>)}</div>
+          <div className="days">{this.state.daysInMonth.map(day => <div className="day">{day}</div>)}</div>
+          <div className="days">{this.state.daysInMonth.map(day => <div className="day">{day}</div>)}</div>
+          <div className="days">{this.state.daysInMonth.map(day => <div className="day">{day}</div>)}</div>
+        </main>
+        <footer>
+          <div className="buttons">
+            <div className="cancel">cancel</div>
+            <div className="ok">ok</div>
+          </div>
+        </footer>
+      </Container>,
+      document.getElementById('portal-root')
+    )
+  }
+}
+
+export default DatePicker;
